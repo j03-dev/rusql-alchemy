f69090672d6c4fd531dc591ee7dca9f585b527eb
j03-dev <24nomeniavo@gmail.com>
2024-08-22 12:55:24 +0300

    add some trait to Boolean

--- src/types.rs
+++ src/types.rs
@@ -7,3 +7,29 @@ pub type Float = f64;
 pub type Date = String;
 pub type DateTime = String;
 pub type Boolean = i32;
+
+pub trait True {
+    fn r#true() -> i32 {
+        1
+    }
+}
+
+pub trait False {
+    fn r#false() -> i32 {
+        0
+    }
+}
+
+pub trait IsTrue {
+    fn is_true(&self) -> bool;
+}
+
+impl True for Boolean {}
+
+impl False for Boolean {}
+
+impl IsTrue for Boolean {
+    fn is_true(&self) -> bool {
+        *self == 1
+    }
+}
